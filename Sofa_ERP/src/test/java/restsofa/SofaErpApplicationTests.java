3880fd12cfa22ae11631b0896d26f58307919aa0
Alberto <albertosaboya@gmail.com>
2024-03-22 21:12:30 +0100

    carpeta eclipse

---
+++ Sofa_ERP/src/test/java/restsofa/SofaErpApplicationTests.java
@@ -0,0 +1,13 @@
+package restsofa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofaErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
