3630b8f2bfac847ecc7824eda8f942385ff32283
Anabella <anabella.aceto@gmail.com>
2024-03-28 09:36:17 +0100

    sofaMaterialService creado

---
+++ Sofa_ERP/src/main/java/restsofa/repository/SofaMaterialRepository.java
@@ -0,0 +1,9 @@
+package restsofa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import restsofa.modelo.entities.SofaMaterial;
+
+public interface SofaMaterialRepository extends JpaRepository<SofaMaterial, Integer>{
+
+}
