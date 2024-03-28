65983836f440ba1e56c0eb800d3d846d40cae4cb
Alberto <albertosaboya@gmail.com>
2024-03-28 14:41:06 +0100

    PedidoRestController

--- Sofa_ERP/src/main/java/restsofa/restcontroller/PedidoRestController.java
+++ Sofa_ERP/src/main/java/restsofa/restcontroller/PedidoRestController.java
@@ -1,5 +1,6 @@
 package restsofa.restcontroller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -27,84 +28,117 @@ import restsofa.service.SofaService;
 @RequestMapping("/pedido")
 
 public class PedidoRestController {
-	
+
 	@Autowired
 	private PedidoService pedidoService;
-	
+
 	@Autowired
 	private ModelMapper modelMapper;
-	
+
 	@Autowired
 	private SofaService sofaService;
-	
+
 	@Autowired
 	private EmpleadoService empleadoService;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
-	
+
 	/*
-	* Método que devuelve todos los pedidos
-	*/
-	
+	 * Método que devuelve todos los pedidos
+	 */
+
 	@GetMapping({ "", "/" })
-	public List<Pedido> todos(){
-		return pedidoService.buscarTodosPedidos();
+	public ResponseEntity<?> todos() {
+
+		try {
+			List<Pedido> lista = pedidoService.buscarTodosPedidos();
+
+			List<PedidoDto> listaDto = new ArrayList<>();
+			for (Pedido pedido : lista) {
+				listaDto.add(modelMapper.map(pedido, PedidoDto.class));
+			}
+
+			return ResponseEntity.ok(listaDto.isEmpty() ? "No hay pedidos disponibles" : listaDto);
+		} catch (Exception e) {
+			return ResponseEntity.status(400).body("Error al cargar la lista de pedidos");
+		}
 	}
-	
+
 	/*
-	* Método que devuelve un pedido
-	*/
-	
+	 * Método que devuelve un pedido
+	 */
+
 	@GetMapping("/{idPedido}")
-	public Pedido uno(@PathVariable int idPedido) {
-		return pedidoService.buscarPedido(idPedido);
+	public ResponseEntity<?> uno(@PathVariable int idPedido) {
+
+		Pedido pedido = pedidoService.buscarPedido(idPedido);
+
+		if (pedido != null) {
+
+			PedidoDto pedidoDto = modelMapper.map(pedido, PedidoDto.class);
+
+			return ResponseEntity.status(200).body(pedidoDto);
+		} else
+			return ResponseEntity.status(400).body("Error, no se encuentra el pedido");
 	}
-	
+
 	/*
-	* Método que da de alta un pedido
-	*/
-	
+	 * Método que da de alta un pedido
+	 */
+
 	@PostMapping("/alta")
 	public ResponseEntity<?> alta(@RequestBody PedidoDto pedidoDto) {
-		
+
 		Pedido pedido = new Pedido();
 		modelMapper.map(pedidoDto, pedido);
-		
+
 		pedido.setSofa(sofaService.buscarSofa(pedidoDto.getIdSofa()));
 		pedido.setCliente(clienteService.buscarCliente(pedidoDto.getIdCliente()));
 		pedido.setVendedor(empleadoService.buscarUno(pedidoDto.getIdEmpleado()));
-		
-		if(pedidoService.altaPedido(pedido) != null) {
+
+		if (pedidoService.altaPedido(pedido) != null) {
 			pedidoDto.setIdPedido(pedido.getIdPedido());
-			return ResponseEntity.status(200).body("Pedido procesado correctamente "+pedido);
-		}
-		else
+			return ResponseEntity.status(200).body("Pedido procesado correctamente " + pedido);
+		} else
 			return ResponseEntity.status(400).body("Error al procesar el pedido");
 	}
-	
+
 	/*
-	* Método que modifica un pedido
-	*/
-	
+	 * Método que modifica un pedido
+	 */
+
 	@PutMapping("/modificar")
-	public Pedido modificar(@RequestBody Pedido pedido) {
-		
-		return pedidoService.modifPedido(pedido);
-		
+	public ResponseEntity<?> modificar(@RequestBody PedidoDto pedidoDto) {
+
+		Pedido pedido = pedidoService.buscarPedido(pedidoDto.getIdPedido());
+
+		if (pedido != null) {
+			modelMapper.map(pedidoDto, PedidoDto.class);
+			pedido.setCliente(clienteService.buscarCliente(pedidoDto.getIdCliente()));
+			pedido.setVendedor(empleadoService.buscarUno(pedidoDto.getIdEmpleado()));
+			pedido.setSofa(sofaService.buscarSofa(pedidoDto.getIdSofa()));
+
+			pedidoService.modifPedido(pedido);
+			return ResponseEntity.status(200).body("Pedido modificado correctamente");
+		} else
+			return ResponseEntity.status(400).body("No se puede modificar el pedido");
 	}
-	
+
 	/*
-	* Método que borra un pedido
-	*/
-	
+	 * Método que borra un pedido
+	 */
+
 	@DeleteMapping("/borrar/{idPedido}")
-	public String borrar (@PathVariable int idPedido) {
-		if(pedidoService.borrarPedido(idPedido))
-			return "Pedido eliminado correctamente";
-		else
-			return "Pedido no se ha podido eliminar";
+	public ResponseEntity<?> borrar(@PathVariable int idPedido) {
+
+		Pedido pedido = pedidoService.buscarPedido(idPedido);
+
+		if (pedido != null) {
+			pedidoService.borrarPedido(idPedido);
+			return ResponseEntity.status(200).body("Pedido eliminado correctamente");
+		} else
+			return ResponseEntity.status(400).body("Pedido no se ha podido eliminar");
 	}
 
 }
